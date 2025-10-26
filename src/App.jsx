2f4cd7ff1abbc233b4ad86d7b1237363bd110bff
Marvyn R <marvynpro@hotmail.com>
2025-10-26 14:26:57 +0100

    little update

--- src/App.jsx
+++ src/App.jsx
@@ -90,7 +90,7 @@ useEffect(() => {
         <div className='wrapper'>
           <header>
             <img src="./hero1.png" alt="Hero Banner" />
-            <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the hassle</h1>
+            <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy</h1>
 
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           </header>
